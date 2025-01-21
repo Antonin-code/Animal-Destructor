67d1ca6fe98748cca3c4881e655b3024141cc1c6
Antonin <antonin.maubert@edu.esiee-it.fr>
2025-01-21 14:08:46 +0100

    ajout pseudos avant debut de partie

--- Main.java
+++ Main.java
@@ -1,13 +1,19 @@
 import java.util.*;
-
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * classe Main
  */
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        // Execute la fonction debut dans le main
         Menu.Debut();
+        List<String> listeJoueurs = Menu.Pseudo();
+        System.out.println(listeJoueurs);
+
+       // Jeu.jeu();
 
 //        while (true) {
 //
--- Menu.java
+++ Menu.java
@@ -1,10 +1,11 @@
-import java.awt.desktop.QuitHandler;
-import java.util.Scanner;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Menu {
 
     // Fonction pour lancer le menu avec les options
-    public static void Debut(){
+    public static void Debut() {
         System.out.println(" ");
         System.out.println(" ");
         System.out.println("Bonjour bienvenue dans Animal-Destructor !! ");
@@ -34,14 +35,15 @@ public class Menu {
             Quitter();
         }
         // Rappele la fonction si le choix est different de P/R/Q
-        else{
+        else {
             System.out.println("Veuillez bien relire les options possibles : ");
             System.out.println(" ");
             Debut();
         }
     }
+
     // Fonction pour lancer le jeu
-    public static void Jouer(){
+    public static void Jouer() {
         System.out.println("Jouer");
     }
 
@@ -72,11 +74,49 @@ public class Menu {
             Quitter();
         }
     }
-        // Fonction pour quitter le jeu
-        public static void Quitter(){
-            System.out.println("Bah reviens pas ");
-            System.exit(0);
 
-        }
+    // Fonction pour quitter le jeu
+    public static void Quitter() {
+        System.out.println("Bah reviens pas ");
+        System.exit(0);
+
 
     }
+
+
+    public static int nb_joueurs() {
+        System.out.println("Combien ya t'ils de joueurs (entre 2 et 4) : ");
+        Scanner scan = new Scanner(System.in);
+        int nbJoueurs = scan.nextInt();
+        if (nbJoueurs == 2 || nbJoueurs == 3 || nbJoueurs == 4) {
+            System.out.println(" ");
+        } else {
+            System.out.println("Veuillez entrer un nombre valide !!");
+            nb_joueurs();
+        }
+        return nbJoueurs;
+    }
+
+
+    public static List<String> Pseudo() {
+        int TotalJoueurs = Menu.nb_joueurs();
+        List<String> maListe = new ArrayList<>();
+        for (int i = 0; i < TotalJoueurs; i++) {
+            System.out.println("Entre un pseudo valide");
+            Scanner scan = new Scanner(System.in);
+            String pseudo = scan.nextLine();
+            if (pseudo.length() >= 2 && pseudo.length() <= 10) {
+                System.out.println("ok");
+                maListe.add(pseudo);
+            } else if (pseudo.length() > 10) {
+                System.out.println("Le pseudo ne doit pas dépasser 10 caractères");
+                Pseudo();
+            } else if (pseudo.length() < 2) {
+                System.out.println("Le pseudo doit contenir au moins 2 caractères");
+                Pseudo();
+            }
+        }
+        System.out.println("La partie commence ! ");
+        return maListe;
+    }
+}
